ae1bcf531b49affc900c7acbeb27395598b05f78
Mark Packham <markpackham1@gmail.com>
2019-12-04 17:47:00 +0000

    alternate socket.io project learned from https://www.youtube.com/playlist?list=PL4cUxeGkcC9i4V-_ZVwLmOusj8YAUhj_9

---
+++ OtherSocketIOProjects/SocketIO2/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const socket = require("socket.io");
+
+// App setup
+const app = express();
+const server = app.listen(4000, function() {
+  console.log("Listening to request on port 4000");
+});
+
+// Static files
+app.use(express.static("public"));
+
+// Socket setup
+const io = socket(server);
+
+io.on("connection", function(socket) {
+  console.log("made socket connection", socket.id);
+  socket.on("chat", function(data) {
+    io.sockets.emit("chat", data);
+  });
+  socket.on("typing", function(data) {
+    socket.broadcast.emit("typing", data);
+  });
+});
